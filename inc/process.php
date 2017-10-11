2aec3e2eabb8dd46778a9a1acd873d39310da06b
Alexis <alexismoreau2@live.fr>
2017-10-11 14:23:43 +0200

    set up form

--- inc/process.php
+++ inc/process.php
@@ -5,27 +5,26 @@
 //POST - in case user does not support javascript, we'll use POST instead
 $name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
 $email = ($_GET['email']) ?$_GET['email'] : $_POST['email'];
-$website = ($_GET['website']) ?$_GET['website'] : $_POST['website'];
 $comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
 
 //flag to indicate which method it uses. If POST set it to 1
 if ($_POST) $post=1;
 
 //Simple server side validation for POST data, of course, you should validate the email
-if (!$name) $errors[count($errors)] = 'Please enter your name.';
-if (!$email) $errors[count($errors)] = 'Please enter your email.'; 
-if (!$comment) $errors[count($errors)] = 'Please enter your comment.'; 
+if (!$name) $errors[count($errors)] = 'Veuillez entrer votre nom';
+if (!$email) $errors[count($errors)] = 'Veuillez entrer votre email.'; 
+if (!$comment) $errors[count($errors)] = 'Veuillez entrer votre commentaire'; 
 
 //if the errors array is empty, send the mail
 if (!$errors) {
 
 	//recipient - YOUR EMAIL.. or whatever
-	$to = 'Your Name <arafa.designer@gmail.com>';	
+	$to = 'Morman Design <morman.design@gmail.com>';	
 	//sender - from the form
 	$from = $name . ' <' . $email . '>';
 	
 	//subject and the html message
-	$subject = 'Comment from ' . $name;	
+	$subject = 'Message de ' . $name;	
 	$message = '
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -33,10 +32,9 @@ if (!$errors) {
 	<head></head>
 	<body>
 	<table>
-		<tr><td>Name</td><td>' . $name . '</td></tr>
+		<tr><td>Nome</td><td>' . $name . '</td></tr>
 		<tr><td>Email</td><td>' . $email . '</td></tr>
-		<tr><td>Website</td><td>' . $website . '</td></tr>
-		<tr><td>Comment</td><td>' . nl2br($comment) . '</td></tr>
+		<tr><td>Commentaire</td><td>' . nl2br($comment) . '</td></tr>
 	</table>
 	</body>
 	</html>';
@@ -46,8 +44,8 @@ if (!$errors) {
 	
 	//if POST was used, display the message straight away
 	if ($_POST) {
-		if ($result) echo 'Thank you! We have received your message.';
-		else echo 'Sorry, unexpected error. Please try again later';
+		if ($result) echo 'Merci! Nous avons reçu votre email !';
+		else echo 'Désolé, nous avons rencontré une erreur. Veuillez réessayer plus tard';
 		
 	//else if GET was used, return the boolean value so that 
 	//ajax script can react accordingly
@@ -60,7 +58,7 @@ if (!$errors) {
 } else {
 	//display the errors message
 	for ($i=0; $i<count($errors); $i++) echo $errors[$i] . '<br/>';
-	echo '<a href="form.php">Back</a>';
+	echo '<a href="form.php">Retour</a>';
 	exit;
 }
 
